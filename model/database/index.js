c44ae8bd20166bd482aa24a94f8e43fb161c8d5e
Leo Ruiz <ruizruizleo@hotmail.com>
2022-03-30 21:16:59 -0300

    teste

--- model/database/index.js
+++ model/database/index.js
@@ -1,9 +1,12 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_BASE, process.env.DB_USER,process.env.DB_PASS, {
-  host:  process.env.DB_HOST,
-  port: 5432,
-  dialect: 'postgres'
-});
+const sequelize = new Sequelize(
+  database_url,
+  {
+    ssl : true,
+    dialectOptions : {
+      ssl: true,
+    },
+  });
 
 module.exports = sequelize;
