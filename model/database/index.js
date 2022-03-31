8950ac1ff65b6f39cfcee83e3ce475792a0f6c26
Leo Ruiz <ruizruizleo@hotmail.com>
2022-03-30 21:01:24 -0300

    porta

--- model/database/index.js
+++ model/database/index.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DB_BASE, process.env.DB_USER,process.env.DB_PASS, {
   host:  process.env.DB_HOST,
-  port: 5433,
+  port: 5432,
   dialect: 'postgres'
 });
 
